ab03d82704f797c0b756fe127a61660bc7b661f1
Chadvik <satvikrao2004@gmail.com>
2024-03-02 20:11:47 +0530

    up3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./pages/home/Home";
 import Header from "./components/Header/Header";
 import MovieList from "./components/MovieList/MovieList";
 import MovieDetail from "./pages/Movie_detail/MovieDetail";
-// import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
 import Search from "./pages/SearchBar/Search";
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
         <Route path="/movie/:id" element={<MovieDetail />} />
         <Route path="/movies/:type" element={<MovieList />} />
         <Route path="/search/*" element={<Search />} />
-        {/* <Route path="/*" element={<ErrorPage />} /> */}
+        <Route path="/error" element={<ErrorPage />} />
       </Routes>
     </div>
   );
